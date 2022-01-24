b946c57e9ec8b37821e19abc92d6a9364bd2d436
Christian Freeman <freemanchristian410@gmail.com>
2022-01-24 11:01:31 -0600

    loan report

--- acquire.py
+++ acquire.py
@@ -1,14 +1,14 @@
 import pandas as pd
-
+import os 
 
 def get_loan_data():
     '''
     This function reads csv stored in the computer or ask the user to fetch the data from kaggle.
     '''
-    filename = "SBAnational.csv"
+    filename = "Loan_Modelling.csv"
 
     if os.path.isfile(filename):
-        return pd.read_csv(filename, index_col=0)
+        return pd.read_csv(filename)
     else:
         print('file not fount locally , please get it from \
               https://www.kaggle.com/mirbektoktogaraev/should-this-loan-be-approved-or-denied')
